cbd46e54a98e01f64e5e4e9e6c305f13613caa65
ryu <ryu@pop-os.localdomain>
2020-10-22 11:11:12 +0300

    set up standard flutter app

---
+++ android/app/src/main/kotlin/com/example/baggage_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baggage_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:baggage_shop/myapp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
